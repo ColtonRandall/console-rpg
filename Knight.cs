59aab38da9c020e260841f4d6709eab4eb994c96
Colton <colton.randall@outlook.com>
2022-10-10 22:19:15 +1300

    added base structure of game

---
+++ Knight.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rpg_game
+{
+    class Knight : Character
+    {
+        public string GetName()
+        {
+            string name = Console.ReadLine();
+            return name;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using rpg_game;
+
+namespace rpgGame
+{
+    /*
+     * Considerations:
+     * - Player to choose a character
+     * - Characters have different health and attack stats (interface?)
+     * - Create enemies to fight (Interface?)
+     * - Allow player to roll a dice (randomise 1-6)
+     * - Dependin on the roll, the player will move forward, encounter an enemy, or stumble upon treasure
+     * - When facing an enemey, the player can either fight or run
+     * - FIghting an enemy = randomise enemey health out of 100 and randomise attack points from both player and enemy
+     * - Defeating an enemy = XP and chance of dropping a health potion
+     * 
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Knight knight = new Knight(); 
+
+            Console.WriteLine("Welcome to the game, please choose your character: ");
+            Console.WriteLine("Knight");
+            Console.WriteLine("Assassin");
+            Console.WriteLine("Wizard");
+
+            Console.WriteLine("Your player name is: " + knight.GetName());
+        } 
+
+    }
+}
